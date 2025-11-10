41fe0003a568f12121cfdd3d44082cd2d6776d63
jaehyeon <jaehyeon4832@gmail.com>
2025-11-10 21:38:45 +0900

    카테고리선택기능구현

--- app/spendingPost.tsx
+++ app/spendingPost.tsx
@@ -1,9 +1,10 @@
 import AntDesign from '@expo/vector-icons/AntDesign';
 import Feather from '@expo/vector-icons/Feather';
 import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import DateTimePicker from '@react-native-community/datetimepicker';
 import { useNavigation } from 'expo-router';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { StatusBar, StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 
@@ -15,7 +16,12 @@ export default function SpendingPost() {
     const [mode, setMode] = useState('date');
     const [show, setShow] = useState(false);
     const [catergorySelectActivated, setCatergorySelectActivated] = useState(false);
-    const [categories, setCategories] = useState(['식비', '자기계발']);
+    const [categories, setCategories] = useState([]);
+    const [plusCategory, setPlusCategory] = useState(false);
+    const [writingPlusCategory, setWritingPlusCategory] = useState('');
+    const [isActiveCategoryDelete, setIsActiveCategoryDelete] = useState(false);
+    
+    const [selectedCategory, setSelectedCategory] = useState('');
 
     const year = date.getFullYear();
     const month = date.getMonth() + 1;
@@ -37,6 +43,28 @@ export default function SpendingPost() {
     const showDatepicker = () => {
         showMode('date');
     };
+
+    const storeCategories = async (plusCategories) => {
+        try {
+            const jsonValue = JSON.stringify(plusCategories);
+            await AsyncStorage.setItem('my_all_categories', jsonValue);
+        } catch(e) {
+            // save error
+        }
+    }
+
+    const getAllCategories = async () => {
+        try {
+            const jsonValue = await AsyncStorage.getItem('my_all_categories');
+            jsonValue != null ? setCategories(JSON.parse(jsonValue)) : null
+        } catch(e) {
+            // read error
+        }
+    }
+
+    useEffect(() => {
+        getAllCategories();
+    }, [])
 
     return (
         <View style={{flex: 1, marginTop: insets.top + 10, backgroundColor: "#F5F5F7"}}>
@@ -62,7 +90,7 @@ export default function SpendingPost() {
                         activeOpacity={0.6}
                         onPress={showDatepicker}
                     >
-                        <Text>{`${year}-${String(month).padStart(2, "0")}-${String(day  ).padStart(2, "0")} ${daysOfWeek[dayOfWeek]}`}</Text>
+                        <Text>{`${year}-${String(month).padStart(2, "0")}-${String(day).padStart(2, "0")} ${daysOfWeek[dayOfWeek]}`}</Text>
                         <MaterialIcons name="date-range" size={24} color="#B1B1B1" />
                     </TouchableOpacity>
                     {show && (
@@ -82,7 +110,11 @@ export default function SpendingPost() {
                         activeOpacity={0.6}
                         onPress={() => {setCatergorySelectActivated(true)}}
                     >
-                        <Text style={styles.categorySelectText}>카테고리를 선택해주세요.</Text>
+                        {
+                            selectedCategory === '' 
+                                ? <Text style={{color: "#b1b1b1"}}>카테고리를 선택해주세요.</Text>
+                                : <Text>{selectedCategory}</Text>
+                        }
                     </TouchableOpacity>
                 </View>
                 <View style={styles.detailContainer}>
@@ -129,23 +161,106 @@ export default function SpendingPost() {
                                 <View style={styles.categoryListContainer}>
                                 {
                                     categories.map((category, index) => (
-                                            <View
+                                            <TouchableOpacity
                                                 key={index}
                                                 style={styles.categoryItem}
+                                                activeOpacity={0.6}
+                                                onPress={() => {
+                                                    setSelectedCategory(category)
+                                                    setCatergorySelectActivated(false)
+                                                }}
                                             >
                                                 <Text>{category}</Text>
-                                            </View>
+                                                {
+                                                    isActiveCategoryDelete
+                                                        ? (
+                                                            <TouchableOpacity
+                                                                style={styles.categoryItemDeleteButton}
+                                                                activeOpacity={0.5}
+                                                                onPress={() => {
+                                                                    setCategories(categories.splice(index, 1))
+                                                                    storeCategories(categories)
+                                                                    getAllCategories()
+                                                                }}
+                                                            >
+                                                                <AntDesign name="close" size={12} color="white" />
+                                                            </TouchableOpacity>
+                                                        )
+                                                        : null
+                                                }
+                                            </TouchableOpacity>
                                     ))
                                 }
+                                    {
+                                        isActiveCategoryDelete || categories.length > 14
+                                             ? null
+                                             : (
+                                                <TouchableOpacity
+                                                    style={styles.categoryItem}
+                                                    activeOpacity={0.5}
+                                                    onPress={() => setPlusCategory(true)}
+                                                >
+                                                    <AntDesign name="plus" size={20} color="black" />
+                                                </TouchableOpacity>
+                                             )
+                                    }
+                                </View>
+                                <View style={styles.categortDeleteActiveButton}>
                                     <TouchableOpacity
-                                        style={styles.categoryItem}
-                                        activeOpacity={0.6}
-                                        onPress={() => setCategories([...categories, '공과금'])}
+                                        onPress={() => {
+                                            isActiveCategoryDelete
+                                                ? setIsActiveCategoryDelete(false)
+                                                : setIsActiveCategoryDelete(true)
+                                        }}
                                     >
-                                        <AntDesign name="plus" size={20} color="black" />
+                                        {
+                                            isActiveCategoryDelete
+                                                ? <AntDesign name="undo" size={24} color="black" />
+                                                : <Feather name="trash-2" size={24} color="black" />
+                                        }
                                     </TouchableOpacity>
                                 </View>
                             </View>
+                            {plusCategory 
+                                ? (
+                                    <View style={styles.categoryPlusContainer}>
+                                        <View style={styles.categoryPlusTitleContainer}>
+                                            <Text style={styles.categoryPlusTitle}>카테고리 작성</Text>
+                                        </View>
+                                        <View style={styles.categoryPlusInputContainer}>
+                                            <TextInput
+                                                style={styles.categoryPlusInput}
+                                                onChangeText={(text) => setWritingPlusCategory(text)}
+                                                value={writingPlusCategory}
+                                                placeholder='추가할 카테고리를 입력해주세요.'
+                                            />
+                                        </View>
+                                        <View style={styles.categoryPlusButtons}>
+                                            <TouchableOpacity
+                                                style={styles.categoryPlusCancelButton}
+                                                onPress={() => {
+                                                    setWritingPlusCategory('')
+                                                    setPlusCategory(false)
+                                                }}
+                                            >
+                                                <Text>취소</Text>
+                                            </TouchableOpacity>
+                                            <TouchableOpacity
+                                                style={styles.categoryPlusConfirmButton}
+                                                onPress={() => {
+                                                    storeCategories([...categories, writingPlusCategory])
+                                                    getAllCategories();
+                                                    setWritingPlusCategory('')
+                                                    setPlusCategory(false)
+                                                }}
+                                            >
+                                                <Text style={{color: "white"}}>저장</Text>
+                                            </TouchableOpacity>
+                                        </View>
+                                    </View>
+                                )
+                                : null
+                            }
                         </View>
                     )
                     : null
@@ -259,8 +374,8 @@ const styles = StyleSheet.create({
         backgroundColor: "white",
         width: "100%",
         height: "68%",
-        borderTopLeftRadius: 36,
-        borderTopRightRadius: 36,
+        borderTopLeftRadius: 24,
+        borderTopRightRadius: 24,
     },
     categorySelectActiveHeader: {
         flexDirection: "row",
@@ -274,13 +389,87 @@ const styles = StyleSheet.create({
         paddingHorizontal: 16,
     },
     categoryItem: {
-        width: '28%',
+        width: '29%',
         height: 50,
-        marginRight: "5%",
+        marginHorizontal: "2%",
         backgroundColor: '#e0e0e0',
         borderRadius: 12,
         justifyContent: 'center',
         alignItems: 'center',
-        marginBottom: 10,
+        marginBottom: "4%",
+    },
+    categoryItemDeleteButton: {
+        position: "absolute",
+        top: -6,
+        right: -6,
+        backgroundColor: "black",
+        width: 24,
+        height: 24,
+        justifyContent: "center",
+        alignItems: "center",
+        borderRadius: 50
+    },
+    categortDeleteActiveButton: {
+        width: "100%", marginTop: 24, paddingHorizontal: 36, alignItems: "flex-end"
+    },
+    categoryPlusContainer: {
+        position: "absolute",
+        bottom: "60%",
+        left: "10%",
+        width: "80%",
+        height: "30%",
+        backgroundColor: "white",
+        borderRadius: 20,
+        alignItems: "center",
+        elevation: 10,
+    },
+    categoryPlusTitleContainer: {
+        flex: 3,
+        justifyContent: "center",
+        alignItems: "center",
+        width: "100%",
+    },
+    categoryPlusTitle: {
+        fontSize: 20,
+        fontWeight: "bold",
+        justifyContent: "center",
+    },
+    categoryPlusInputContainer: {
+        flex: 3,
+        justifyContent: "center",
+        alignItems: "center",
+        width: "100%",
+    },
+    categoryPlusInput: {
+        backgroundColor: "#f5f5f7",
+        width: "80%",
+        height: "80%",
+        borderRadius: 12,
+        textAlign: "center",
+        fontSize: 14,
+    },
+    categoryPlusButtons: {
+        flex: 4,
+        width: "100%",
+        flexDirection: "row",
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    categoryPlusCancelButton: {
+        backgroundColor: "#f5f5f7",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "60%",
+        width: "38%",
+        borderRadius: 10,
+        marginRight: "4%",
+    },
+    categoryPlusConfirmButton: {
+        backgroundColor: "black",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "60%",
+        width: "38%",
+        borderRadius: 10,
     }
 })
